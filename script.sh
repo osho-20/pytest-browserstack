bf2b107e3375731dd00d1420a8968ff3e26a8f76
YASH JAIN <yashjainiiita@gmail.com>
2026-02-04 19:42:04 +0530

    script fix

--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
+echo "starting pytest testing"
+echo "creating virtual environment and installing dependencies"
 python -V
 python -m venv env
 source env/bin/activate
+echo "virtual environment created and activated"
+echo "installing dependencies from requirements.txt"
 pip install -r requirements.txt
+echo "dependencies installed"
+echo "running pytest for bstack-sample-test.py"
 browserstack-sdk pytest ./tests/bstack-sample-test.py
+echo "pytest testing completed"
